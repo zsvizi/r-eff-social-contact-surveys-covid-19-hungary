2963eba093bc1202070aaee82e5dd38790dadcd4
Zsolt Vizi <vizizsolt89@gmail.com>
2021-06-02 10:00:01 +0200

    Reorder imports

--- dash/app.py
+++ dash/app.py
@@ -5,16 +5,16 @@ from datetime import datetime
 import sys
 
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
 import dash_bootstrap_components as dbc
+import dash_core_components as dcc
 import dash_daq as daq
 from dash.dependencies import Input, Output, State
-import plotly.graph_objects as go
+import dash_html_components as html
 import plotly.express as px
+import plotly.graph_objects as go
 
-import matplotlib.pyplot as plt
 from matplotlib.colors import to_hex
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
